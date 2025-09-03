7b9fd143cf3b4d7b6b86b7420534d951f4aa0cb7
gitsofpranav <dewanganpranav2@gmail.com>
2025-09-03 20:21:44 +0530

    middleware

--- src/models/user.moel.js
+++ src/models/user.moel.js
@@ -1,6 +1,6 @@
 import mongoose,{Schema} from "mongoose";
-import { JsonWebTokenError } from "jsonwebtoken";
-import bcrypt, { hash } from "bcrypt"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
     {
