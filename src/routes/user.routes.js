2adacc3f7d8018301bc6ee2ee8a03d5542b1a00f
gitsofpranav <dewanganpranav2@gmail.com>
2025-09-14 05:56:48 +0530

    Add user account management routes: change password, current user, update account, and manage avatars

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { loginUser, logOutUser, registerUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logOutUser, 
+    registerUser,
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateuserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHisory 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,6 +36,13 @@ router.route("/register").post(
    //secured route
    router.route("/logout").post(verifyJWT,logOutUser)
    router.route("/refresh-token").post(refreshAccessToken)
+   router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+   router.route("/current-user").get(verifyJWT,getCurrentUser)
+   router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+   router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateuserAvatar)
+   router.route("/cover-image").patch(verifyJWT,updateAccountDetails.single("/coverImage"),updateUserCoverImage)
+   router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+   router.route("/history"),get(verifyJWT,getWatchHisory)
    
    
 export default router
