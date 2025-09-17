b8f0e9ddcb9688beaf860d8a6b91435456d08f63
gitsofpranav <dewanganpranav2@gmail.com>
2025-09-17 18:35:26 +0530

    Enhance updateTweet functionality with error handling and response structure

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -94,8 +94,33 @@ const updateTweet = asyncHandler(async (req, res) => {
         throw new ApiError(401, "Unauthorized user")
     }
 
-    const { content } = req.body
-    
+    const { tweetId,content } = req.body
+    if(!tweetId || !content){
+        throw new ApiError(400, "Tweet ID and content are required")
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        {
+            _id:tweetId,owner: user._id
+        },
+        {
+            $set: {
+                content: content
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!updateTweet){
+        throw new ApiError(404,"Tweet not found or not authorized")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,updateTweet,"Tweet updated successfully"))
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
